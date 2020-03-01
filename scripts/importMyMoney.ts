b2ce883ab63b911d18f43109b2dcc6d2bacac146
zhuangshifu <wangz.today@gmail.com>
2020-03-01 22:49:52 +0800

    add: 引入 aws sdk， 配置本地credentials， promise put to dynamodb features

--- scripts/importMyMoney.ts
+++ scripts/importMyMoney.ts
@@ -5,12 +5,65 @@
  */
 
 const csv2json = require("csvtojson");
+import * as async from 'async';
+import * as AWS from 'aws-sdk';
+
+const credentials = new AWS.SharedIniFileCredentials({profile: 'sisi-account'});
+
+AWS.config.credentials = credentials;
+AWS.config.update({region: 'ap-east-1'});
+
 
 const csvFilePath = './data/mymoney.csv';
 
+//
+// csv2json()
+//     .fromFile(csvFilePath,
+//         (err, result) => {
+//             console.log(result);
+//         });
 
-csv2json()
-    .fromFile(csvFilePath,
-        (err, result) => {
-            console.log(result);
-        });
+// 下面是主要尝试一下 把 aws - sdk 拿来使用
+
+const ddb = new AWS.DynamoDB();
+
+const params = {
+    TableName: 'mymoney',
+    Item: {
+        'uuid': {S: '001'},
+        'CUSTOMER_NAME': {S: 'Richard Roe'}
+    }
+};
+
+// Call DynamoDB to add the item to the table
+// ddb.putItem(params, function (err, data) {
+//     if (err) {
+//         console.log("Error", err);
+//     } else {
+//         console.log("Success", data);
+//     }
+// });
+/**
+ * 下面这个是 用 promise 的方式来解决这个问题
+ * @param data
+ */
+const putData = async (data) => {
+    try {
+        const result = await ddb.putItem(data).promise();
+        console.log(' resultLLL');
+        return result;
+    } catch (error) {
+        console.log('error: ',);
+    }
+};
+
+putData(params).then(result=>{
+    // console.log(result)
+}).catch(error=>{
+    console.log('所以有错误：：： ');
+});
+
+
+
+
+
