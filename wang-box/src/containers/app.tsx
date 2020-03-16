b250049aa9ae923e76ce53386b08c5aca42c3722
Wang <wangz.today@gmail.com>
2020-03-16 23:52:27 +0800

    ant design layout not work

--- wang-box/src/containers/app.tsx
+++ wang-box/src/containers/app.tsx
@@ -1,61 +1,22 @@
-/** 2020/3/13
- *   作者: Wang
- *   功能: 整个项目的入口， 目前还包含了 多语言的切换
- */
-
-import {IntlProvider} from "react-intl";
-import {translationMap} from "../utils/translationMap";
-
-import * as React from "react";
-import Dashboard from "./mymoney/dashboard/dashboard";
-
-interface IProps {
-
-}
-
-interface IState {
-    locale: string
-}
+import React from 'react';
 
 
-class App extends React.Component<IProps, IState> {
+class SiderDemo extends React.Component {
+    state = {
+        collapsed: false,
+    };
 
-    constructor(props:IProps) {
-        super(props);
-        // this.state = {
-        //     locale: 'zh'
-        // }
-    }
-
-    componentDidMount(): void {
-        // this.state = {
-        //     locale: 'zh'
-        // }
-    }
-
-    localeChange = () => {
-        let hh_locale = this.state.locale === 'zh' ? 'en' : 'zh'
-        this.setState({locale: hh_locale});
-    }
-
-    translationMap = (locale:string) => translationMap[locale];
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
 
     render() {
-        // const {locale} = this.state;
-        const locale = 'zh'
-
-        let div = <div>
-            {/*Note: 暂时先禁用这个功能吧*/}
-            {/*<Button onClick={() => {*/}
-            {/*    this.localeChange()*/}
-            {/*}}>切换语言</Button>*/}
-            <IntlProvider locale={locale} messages={this.translationMap((locale))}>
-                <Dashboard/>
-            </IntlProvider>
-        </div>;
-        return div
+        return (
+          <div></div>
+        );
     }
 }
 
-export default App;
-
+export default SiderDemo;
